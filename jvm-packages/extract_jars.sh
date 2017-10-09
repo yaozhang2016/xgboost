3f1bc7556eb586da520d4d840e50612d0d9b52f5
mmalohlava <michal@h2o.ai>
2017-10-09 14:42:15 -0700

    Append suffix to generated jar file with library.

--- jvm-packages/extract_jars.sh
+++ jvm-packages/extract_jars.sh
@@ -6,13 +6,13 @@ cat <<EOF
 Extract library from give jar file
 
 $0 <input jar file> <lib suffix>
-  - input jar file: path to jar file with xgboost library 
+  - input jar file: path to jar file with xgboost library
   - lib suffix: suffix to append to xgboost binary library
 
 EOF
 }
 
-OUTDIR="target/h2o" 
+OUTDIR="target/h2o"
 OS=$(uname | sed -e 's/Darwin/osx/' | tr '[:upper:]' '[:lower:]')
 BITS=$(getconf LONG_BIT)
 PLATFORM="${OS}_${BITS}"
@@ -39,13 +39,13 @@ if [ $# -gt 1 ]; then
         # Put library into actual place
         echo "Generating jar file with native libs..."
         mkdir "lib/${PLATFORM}"
-        find lib -type f | while read -r f; do 
+        find lib -type f | while read -r f; do
             fname=$(basename "$f")
             fname=${fname//./$lib_suffix.}
             mv "$f" "lib/${PLATFORM}/$fname"
         done
-        native_lib_jar=${jar_filename//-/-native-${OS}-}
-        jar -cf "${native_lib_jar}" ./lib 
+        native_lib_jar=${jar_filename//-/-native-${OS}${lib_suffix}-}
+        jar -cf "${native_lib_jar}" ./lib
         rm -rf ./lib
     )
 
